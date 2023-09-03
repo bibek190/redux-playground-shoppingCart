98651d6f5fa7a6ff7ca8b2b3e279e9494168948b
Bibek <bibek9818917036@gmail.com>
2023-09-04 02:10:59 +1000

    added async in slice

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,24 +2,35 @@ import { computeHeadingLevel } from "@testing-library/react";
 import React, { useEffect, useState } from "react";
 import { add } from "../store/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { STATUSES, fetchProducts } from "../store/productSlice";
 
 const Products = () => {
   const dispatch = useDispatch();
-  const [products, setProducts] = useState([]);
+  const { data: products, status } = useSelector((state) => state.product);
+  // const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
-      setProducts(data);
-    };
-    fetchProducts();
+    dispatch(fetchProducts());
+
+    // const fetchProducts = async () => {
+    //   const res = await fetch("https://fakestoreapi.com/products");
+    //   const data = await res.json();
+    //   setProducts(data);
+    // };
+    // fetchProducts();
   }, []);
 
   const handleAdd = (product) => {
     dispatch(add(product));
   };
 
+  if (status === STATUSES.LOADING) {
+    return <h1>Loading.......</h1>;
+  }
+
+  if (status === STATUSES.ERROR) {
+    return <h1>Something went wrong</h1>;
+  }
   return (
     <div className="productsWrapper">
       {products.map((product) => {
